f5fabfbe71d9ff364f23d180085e0ba599ad07ad
Rotem Yaari <vmalloc@gmail.com>
2018-03-28 11:44:39 +0300

    Cosmetics

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,15 @@ extern crate libc;
 
 use std::ffi::CString;
 use std::io::Write;
-use std::path::{Path};
+use std::path::Path;
 use libc::{c_char, execv, prctl, PR_SET_PDEATHSIG, SIGTERM};
 use errno::errno;
 
-
 fn resolve_executable(s: &str) -> String {
-
     let path = Path::new(s);
     if !path.is_absolute() {
-
         if let Some(paths) = std::env::var_os("PATH") {
-
             for dir in std::env::split_paths(&paths) {
-
                 let filename = dir.join(&path);
 
                 if filename.is_file() {
@@ -28,19 +23,17 @@ fn resolve_executable(s: &str) -> String {
     s.to_owned()
 }
 
-
 fn main() {
-
     let executable = std::env::args().skip(1).next().unwrap();
 
     let mut args: Vec<CString> = Vec::new();
-    args.push(CString::new(
-        resolve_executable(&executable)
-    ).unwrap());
-    args.extend(std::env::args()
-                .skip(2)
-                .map(|arg| CString::new(arg).unwrap())
-                .collect::<Vec<CString>>());
+    args.push(CString::new(resolve_executable(&executable)).unwrap());
+    args.extend(
+        std::env::args()
+            .skip(2)
+            .map(|arg| CString::new(arg).unwrap())
+            .collect::<Vec<CString>>(),
+    );
 
     let mut c_args = args.iter()
         .map(|arg| arg.as_ptr())
@@ -51,7 +44,6 @@ fn main() {
     // c_env.push(std::ptr::null());
 
     unsafe {
-
         let retval = prctl(PR_SET_PDEATHSIG, SIGTERM);
 
         if retval != 0 {
@@ -62,6 +54,5 @@ fn main() {
         execv(c_args[0], c_args.as_ptr());
         writeln!(std::io::stderr(), "Error executing program: {}", errno()).unwrap();
         std::process::exit(1);
-
     }
 }
